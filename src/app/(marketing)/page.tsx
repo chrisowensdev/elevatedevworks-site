bd3446eb82003878eb3f659e8b33154e62d23f8e
Christopher Owens <cowensdev@gmail.com>
2026-01-14 15:26:48 -0500

    Redesign home page section and hero updates

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,18 +5,18 @@ import CTASection from "@/components/sections/CTASection";
 import ServicesOverview from "./services/_components/ServicesOverview";
 import WorkSection from "./work/_components/WorkSection";
 import FAQSection from "../../components/sections/FAQSection";
+import Problem from "./page/_components/Problem";
+import Plan from "./page/_components/Plan";
 
 export default function Page() {
 	return (
 		<Main>
 			<Hero />
-			<About />
+			<Problem ctaText="Discuss your project" ctaUrl="/Contact" />
+			<Plan ctaText="See services" ctaUrl="/services" />
 			<ServicesOverview ctaText="View services" ctaUrl="/services" />
-			<WorkSection
-				className="bg-white/70"
-				ctaText="View all projects"
-				ctaUrl="/work"
-			/>
+			<WorkSection ctaText="View all projects" ctaUrl="/work" />
+			<About />
 			<FAQSection context="home" />
 			<CTASection />
 		</Main>
